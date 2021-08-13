6ce56c6ba9f7060b3c34b8fd4afba86d721b983d
TimothyJNewman <84350177+TimothyJNewman@users.noreply.github.com>
2021-08-13 04:41:35 +0100

    Made changes to input range and validation

--- script.js
+++ script.js
@@ -5,19 +5,25 @@ function compute() {
   res = document.getElementById("result");
   errorRef = document.getElementById("errorMessage");
   if (p == "" || r == "" || "") {
-    res.innerText = "";
-    errorRef.innerText = "Missing Values";
-    errorRef.style.display = "block";
+    res.style.display = "none";
+    window.alert("Please enter a principle");
+    document.getElementById("principal").focus();
+  } else if (!(parseFloat(p) > 0)) {
+    res.style.display = "none";
+    window.alert("Please enter a positive principle");
+    document.getElementById("principal").focus();
+  } else if (!(parseFloat(r) > 0)) {
+    res.style.display = "none";
+    window.alert("Please enter a positive rate");
+    document.getElementById("rate").focus();
   } else {
-    if (errorRef.style.display == "block") {
-      errorRef.style.display = "none";
-    }
     resultMessage = ""
     resultMessage += "If you deposit <span>" + p + "</span>, \n";
     resultMessage += "at an interest rate of <span>" + r + "%</span>. \n";
     resultMessage += "You will reveive an amount of <span>" + Math.round(p * (r / 100 * t)) + "</span>, \n";
     resultMessage += "in the year <span>" + t + "</span>";
     res.innerHTML = resultMessage;
+    res.style.display = "block";
   }
 
 }
