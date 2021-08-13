561f83197ec14f415a10dd1953ab86747c0918a2
test <test@g.com>
2021-08-13 01:03:39 +0000

    Some logic and styles

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function compute()
-{
+function compute() {
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    t = document.getElementById("years").value;
+    if (p == undefined || r == undefined || undefined) {
+        const errorMessageP = document.createElement("p");
+        const errorMessageText = document.createTextNode("Incomplete Info");
+        errorMessageP.appendChild(errorMessageText);
+        document.body.appendChild(errorMessageP);
+    } else {
+        res = document.getElementById("result");
+        res.value = p * (r / 100 * t);
+    }
+
 }
-        
