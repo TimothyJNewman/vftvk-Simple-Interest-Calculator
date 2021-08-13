9461201644d9c6727e16d1eddbeb60d467470bc2
TimothyJNewman <84350177+TimothyJNewman@users.noreply.github.com>
2021-08-13 03:36:06 +0100

    Styling and error handling. Works

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 function compute() {
-    p = document.getElementById("principal").value;
-    r = document.getElementById("rate").value;
-    t = document.getElementById("years").value;
-    if (p == undefined || r == undefined || undefined) {
-        const errorMessageP = document.createElement("p");
-        const errorMessageText = document.createTextNode("Incomplete Info");
-        errorMessageP.appendChild(errorMessageText);
-        document.body.appendChild(errorMessageP);
-    } else {
-        res = document.getElementById("result");
-        res.value = p * (r / 100 * t);
+  p = document.getElementById("principal").value;
+  r = document.getElementById("rate").value;
+  t = document.getElementById("years").value;
+  res = document.getElementById("result");
+  errorRef = document.getElementById("errorMessage");
+  if (p == "" || r == "" || "") {
+    res.innerText = "";
+    errorRef.innerText = "Missing Values";
+    errorRef.style.display = "block";
+  } else {
+    if (errorRef.style.display == "block") {
+      errorRef.style.display == "none";
     }
+    res.innerText = Math.round(p * (r / 100 * t));
+  }
 
+}
+
+function setSelectedRate() {
+  r = document.getElementById("rate").value;
+  document.getElementById("selectedRate").innerText = r + "% ";
 }
